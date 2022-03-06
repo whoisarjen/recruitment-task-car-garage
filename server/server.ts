913c08d9fa4d61b54fd87cfeb85cbbc6898c58f8
whoisarjen <kamilow97@gmail.com>
2022-03-06 17:57:22 +0100

    cors fix + server env
    fix for cors on server site + adding env to seperate origin's url

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,14 @@
+import dotenv from 'dotenv'
+dotenv.config({ path: `.env` })
 import express from 'express';
 import helmet from 'helmet'
 import routes from './utils/routes.utils';
+import cors from 'cors'
+
+const corsObject = { origin: process.env.ORIGIN, credentials: true }
 
 const app = express();
+app.use(cors(corsObject))
 app.use(helmet())
 routes(app);
 
