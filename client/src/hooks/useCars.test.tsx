1a7be846c2f12fc4bfd3c7b5c9d1fba54c7bc585
whoisarjen <kamilow97@gmail.com>
2022-03-07 00:29:24 +0100

    Update useCars.test.tsx
    Client site is covered with tests

--- client/src/hooks/useCars.test.tsx
+++ client/src/hooks/useCars.test.tsx
@@ -1,20 +1,35 @@
 import { renderHook } from "@testing-library/react-hooks";
 import { wait } from "@testing-library/user-event/dist/utils";
 import useCars from "./useCars";
+import useAxios from './useAxios'
+
+jest.mock('./useAxios', () => () => {
+    return {
+        get: () => {
+            return {
+                data: [{}, {}]
+            }
+        }
+    }
+})
 
 describe('Testing useCars', () => {
+    
+    // It's working bc we are not awaiting so we get the default value = []
     it('Expect to bring empty array', () => {
         const { result } = renderHook(() => useCars())
 
         expect(result.current).toEqual([])
     })
+
     it('Expect to bring array of elements', async () => {
-        // const { result, waitForNextUpdate } = renderHook(() => useCars())
 
-        // await waitForNextUpdate();
+        const { result, waitForNextUpdate } = renderHook(() => useCars())
 
-        // console.log(result.current)
-        // expect(result.current).toEqual([])
+        wait(5000)
+        await waitForNextUpdate()
+
+        expect(result.current).toEqual([{}, {}])
     })
 })
 
