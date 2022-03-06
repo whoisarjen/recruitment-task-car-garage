ea2ee45a204f0ebb185e8f66a4a534fdca3058d5
whoisarjen <kamilow97@gmail.com>
2022-03-06 17:11:00 +0100

    Update query.utils.ts
    GET API '/' is supposted to give us every car according to _date_added_ asc

--- server/utils/query.utils.ts
+++ server/utils/query.utils.ts
@@ -2,7 +2,7 @@ import { db } from './connection.utils'
 
 export const getAll = async () => {
     return new Promise((resolve, reject) => {
-        db.all(`SELECT * FROM warehouses`, [], (err, rows) => {
+        db.all(`SELECT * FROM warehouses ORDER BY list_cars_vehicles_date_added ASC`, [], (err, rows) => {
             if (err) {
                 reject(err);
             }
