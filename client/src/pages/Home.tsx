a59bd1942554376214083141ade1a11006e40a70
whoisarjen <kamilow97@gmail.com>
2022-03-06 17:49:44 +0100

    cors + env
    Adding cors on server site.
    Adding env file on client site.
    + some temporary testing in home

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,4 +1,18 @@
+import { useEffect, useState } from "react";
+import useAxios from "../hooks/useAxios";
+
 const Home = () => {
+    const [cars, setCars] = useState([])
+    const { get } = useAxios()
+
+    useEffect(() => {
+        (async () => {
+            const res = await get({ url: '/' })
+            // setCars(res || [])
+            console.log(res)
+        })()
+    }, [])
+
     return <></>
 }
 
