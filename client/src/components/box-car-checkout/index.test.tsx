5bd9a56f0a7185d9cd3c0742c309cd3e5090683c
whoisarjen <kamilow97@gmail.com>
2022-03-07 03:39:58 +0100

    Update index.test.tsx
    Fixes in test for new property in box

--- client/src/components/box-car-checkout/index.test.tsx
+++ client/src/components/box-car-checkout/index.test.tsx
@@ -1,30 +1,30 @@
 import { render, screen } from "@testing-library/react";
-import BoxCar from ".";
+import BoxCarCheckout from ".";
 import { FakeCar } from "../../utils-test/car.utils";
 
 
 describe('Testing BoxCarCheckout', () => {
     it('Expect to render component with OK', () => {
-        render(<BoxCar {...FakeCar} />)
+        render(<BoxCarCheckout removeCar={jest.fn()} {...{ car: FakeCar }} />)
     })
 
     it('Expect to show make inside box', () => {
-        render(<BoxCar {...FakeCar} />)
+        render(<BoxCarCheckout removeCar={jest.fn()} {...{ car: FakeCar }} />)
         screen.getByText(/test/i)
     })
 
     it('Expect to show model inside box', () => {
-        render(<BoxCar {...FakeCar} />)
+        render(<BoxCarCheckout removeCar={jest.fn()} {...{ car: FakeCar }} />)
         screen.getByText(/kamil/i)
     })
 
     it('Expect to show price inside box', () => {
-        render(<BoxCar {...FakeCar} />)
+        render(<BoxCarCheckout removeCar={jest.fn()} {...{ car: FakeCar }} />)
         screen.getByText(/5000/i)
     })
 
     it('Expect to make box available for licensed = true', () => {
-        render(<BoxCar {...FakeCar} />)
+        render(<BoxCarCheckout removeCar={jest.fn()} {...{ car: FakeCar }} />)
         expect(screen.getByTestId('BoxCar')).toHaveStyle(`background: rgb(47, 59, 82)`)
     })
 
