7c4473bae068123aaed16759f00ccb5986ca9921
whoisarjen <kamilow97@gmail.com>
2022-03-07 02:42:10 +0100

    Checkbox page
    Created checkbox page + routing

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes, useNavigate } from "react-router-dom";
 import Navbar from "./layout/navbar";
+import Checkout from "./pages/checkout";
 import Home from "./pages/Home";
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
         <Navbar navigateTo={navigateTo}>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="/checkout" element={<Checkout />} />
             </Routes>
         </Navbar>
     )
---
+++ client/src/pages/checkout/index.tsx
@@ -0,0 +1,5 @@
+const Checkout = () => {
+    return <></>
+}
+
+export default Checkout;
