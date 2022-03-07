668a136811d37be56b8fae5cec84be127babe1f2
whoisarjen <kamilow97@gmail.com>
2022-03-07 02:16:37 +0100

    Update index.test.tsx

--- client/src/layout/navbar/index.test.tsx
+++ client/src/layout/navbar/index.test.tsx
@@ -1,5 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import Navbar from ".";
+
 describe('Testing Navbar', () => {
-    it.todo('Expect to show children element')
+    it('Expect to show children element', () => {
+        render(<Navbar><button data-testid="button">Button</button></Navbar>)
+
+        screen.getByTestId('button')
+    })
 
     it('Rest is tested by MUI team', () => { })
 })
