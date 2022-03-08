1187e5304f1d18e1f4367f2d6b7f9a7fa395cdf8
whoisarjen <kamilow97@gmail.com>
2022-03-08 04:59:42 +0100

    Update index.tsx
    Perfect case to use Promise.all()

--- client/src/layout/redux/index.tsx
+++ client/src/layout/redux/index.tsx
@@ -2,7 +2,6 @@ import { useEffect } from "react";
 import useCars from "../../hooks/useCars";
 import useCheckout from "../../hooks/useCheckout";
 import { useAppDispatch } from "../../hooks/useRedux";
-import { CheckoutAnyProps } from "../../interfaces/checkout.interface";
 import { addArrayToCars } from "../../redux/slices/car.slice";
 import { addArrayToCheckout } from "../../redux/slices/checkout.slice";
 
@@ -13,10 +12,9 @@ const Redux = ({ children }: { children: any }) => {
 
     useEffect(() => {
         (async () => {
-            const cars = await getAllCarsFromDB()
-            dispatch(addArrayToCars(cars))
-            const checkouts: CheckoutAnyProps[] = await getAllCheckouts()
-            dispatch(addArrayToCheckout(checkouts))
+            const res = await Promise.all([await getAllCarsFromDB(), await getAllCheckouts()])
+            dispatch(addArrayToCars(res[0]))
+            dispatch(addArrayToCheckout(res[1]))
         })()
     })
 
