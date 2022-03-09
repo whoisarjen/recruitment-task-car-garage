060a1af5f56b96ee7189b967e616ba89b9a4dc5a
whoisarjen <kamilow97@gmail.com>
2022-03-09 03:01:14 +0100

    Update index.test.tsx
    pages covered with tests

--- client/src/pages/checkout/index.test.tsx
+++ client/src/pages/checkout/index.test.tsx
@@ -1,3 +1,16 @@
-it.todo('')
+import { screen } from "@testing-library/react";
+import { FakeCar } from "../../utils-test/car.utils";
+import { setupComponent } from "../../utils-test/test.utils";
+import BaseCheckout from "./Checkout";
+
+describe('Testing Checkout', () => {
+    it('Expect to show car in box', () => {
+        setupComponent(<BaseCheckout checkouts={[{...FakeCar, id: 123}]} />)
+
+        screen.findByText(FakeCar.list_cars_vehicles_price)
+        screen.findByText(FakeCar.list_cars_vehicles_make)
+        screen.findByText(FakeCar.list_cars_vehicles_model)
+    })
+})
 
 export default {};
